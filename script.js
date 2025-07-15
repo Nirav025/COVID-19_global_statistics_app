ac1f4309a56fd3722d1032d700ebc137e4a762b6
Nirav025 <niravthakor2002@gmail.com>
2025-07-15 14:57:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*---------------------------- JS Code By Nirav -----------------------------*/
+
+
+
+fetch('https://disease.sh/v3/covid-19/all')
+
+.then(res => res.json())
+
+.then(data => {
+
+    document.getElementById('cases').textContent = data.cases.toLocaleString();
+
+    document.getElementById('deaths').textContent = data.deaths.toLocaleString();
+
+    document.getElementById('recovered').textContent = data.recovered.toLocaleString();
+
+    document.getElementById('active').textContent = data.active.toLocaleString();
+
+})
+
+.catch((err) => console.log("error:", err));
